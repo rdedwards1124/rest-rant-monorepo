ef0b4836191fd415dd6d25144694ef7308caf954
Rashonda Edwards <rashondaedwards@aol.com>
2023-07-27 19:19:13 -0700

    session work

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,27 +1,46 @@
-const router = require('express').Router()
-const db = require("../models")
-const bcrypt = require('bcrypt')
+const router = require("express").Router();
+const db = require("../models");
+const bcrypt = require("bcrypt");
 
-const { User } = db
+const { User } = db;
 
-router.post('/', async (req, res) => {
-    console.log('logged in with', req.body)
-    let {email, password} = req.body
+router.post("/", async (req, res) => {
+    console.log("logged in with", req.body);
+    let { email, password } = req.body;
     let user = await User.findOne({
         where: {
-            email
-        }
-    })
+            email,
+        },
+    });
 
-    if (!user || !await bcrypt.compare(password, user.passwordDigest)){
+    if (!user || !(await bcrypt.compare(password, user.passwordDigest))) {
         res.status(404).json({
-            message: 'Could not find user...'
-        })
+            message: "Could not find user...",
+        });
     } else {
-        res.status(200).json({user})
+        req.session.userId = user.userId;
+        res.status(200).json({ user });
     }
 
-    console.log(user)
-})
+    console.log(user);
+});
 
-module.exports = router
+router.get('/profile', async (req, res) => {
+    let userId = req.session.userId;
+    try {
+        let user = await User.findOne({
+            where: {
+                userId
+            }
+        });
+        if (!user) {
+            res.status(404);
+        } else {
+            res.status(200).json(user);
+        }
+    } catch(e) {
+        res.status(404).json({ message: 'Could not validate user'})
+    }
+});
+
+module.exports = router;
--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,17 +1,35 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
+export const CurrentUser = createContext();
 
-export const CurrentUser = createContext()
+function CurrentUserProvider({ children }) {
+    const [currentUser, setCurrentUser] = useState(null);
 
-function CurrentUserProvider({ children }){
+    window.setCurrentUser = setCurrentUser;
 
-    const [currentUser, setCurrentUser] = useState(null)
-    window.setCurrentUser = setCurrentUser
+    useEffect(() => {
+        const getLoggedInUser = async () => {
+            let response = await fetch(
+                "http://localhost:5000/authentication/profile",
+                {
+                    credentials: "include",
+                }
+            );
+            let user = await response.json();
+            if (response.ok) {
+                setCurrentUser(user);
+            } else {
+                setCurrentUser(null);
+            }
+        };
+        getLoggedInUser();
+    }, []);
+
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
             {children}
         </CurrentUser.Provider>
-    )
+    );
 }
 
-export default CurrentUserProvider
+export default CurrentUserProvider;
